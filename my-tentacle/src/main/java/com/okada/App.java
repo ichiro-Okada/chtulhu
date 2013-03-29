3b15c755706070e0cea83be21d51ae8782286aad
1 <1@1-PC>
2013-03-29 02:29:40 +0200

    file scaner

--- my-tentacle/src/main/java/com/okada/App.java
+++ my-tentacle/src/main/java/com/okada/App.java
@@ -1,4 +1,5 @@
 package com.okada;
+import java.io.File;
 import java.util.UUID;
 
 /**
@@ -13,14 +14,39 @@ public class App
         App obj = new App();
         System.out.println("Unique ID : " + obj.generateUniqueKey());
         System.out.println( "Hello World!" );
+        
+        App fp = new App(); 
+        fp.palpate( fp.genStartDirectory() );
+        System.out.println( "Buy World!" );
     }
  
+    public String genStartDirectory(){
+    	return "h:\\hmm_ost\\";
+    }
+    
     public String generateUniqueKey(){
  
     	String id = UUID.randomUUID().toString();
     	return id;
  
     }
+    
+    public void palpate ( String path ) { 
+
+        File root = new File( path ); 
+        File[] list = root.listFiles(); 
+
+        for ( File f : list ) { 
+            if ( f.isDirectory() ) { 
+            	System.err.println( "Dir:" + f.getAbsoluteFile() ); 
+            	palpate( f.getAbsolutePath() ); 
+                
+            } 
+            else { 
+                System.err.println( "File:" + f.getName() ); 
+            } 
+        } 
+    } 
 
 
 }
